818aac1f855f68aeafcf15d16f9b666a8100e735
cyberSJ <sungjoonkim4.0@gmail.com>
2015-04-18 14:23:26 -0400

    cmake_x: Do not include the same .cmake again.

--- cmake_x/sub_directory/cmake/some_module.cmake
+++ cmake_x/sub_directory/cmake/some_module.cmake
@@ -1,4 +1,13 @@
-message(STATUS "running some_moudle.cmake")
+if (SOME_MODULE)
+    message(STATUS "skipping some_module.cmake")
+    message(STATUS "SOME_MODULE = " ${SOME_MODULE})
+    return()
+else()
+    message(STATUS "running some_module.cmake")
+    set(SOME_MODULE TRUE)
+endif()
+
 if (transferrable_variable)
     message(STATUS "transferrable variable in some_module in sub_directory: " ${transferrable_variable})
 endif()
+
