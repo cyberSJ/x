973b152060ed3eb0428f3686ee56bce99f36aa60
sung <sungjoonkim4.0@gmail.com>
2017-09-19 23:37:50 -0400

    ros_x: Practice adding ROS message.

---
+++ ros_x/catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/cyber/repos/x/ros_x/catkin_ws/src/beginner_tutorials/msg")
+set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
