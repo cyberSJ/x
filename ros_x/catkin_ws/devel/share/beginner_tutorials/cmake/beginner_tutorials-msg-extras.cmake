4028c20aedb5d32b5d08687f1b4929dcc140462f
sung <sungjoonkim4.0@gmail.com>
2017-09-23 15:05:54 -0400

    ros_x: Completely removed catkin_ws/devel directory.

--- ros_x/catkin_ws/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(beginner_tutorials_MESSAGE_FILES "/home/cyber/repos/x/ros_x/catkin_ws/src/beginner_tutorials/msg/Num.msg")
-set(beginner_tutorials_SERVICE_FILES "/home/cyber/repos/x/ros_x/catkin_ws/src/beginner_tutorials/srv/AddTwoInts.srv")
