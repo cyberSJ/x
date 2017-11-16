39849aba736306201824dcbe667c8cc361839188
sung <sungjoonkim4.0@gmail.com>
2017-11-15 21:34:39 -0500

    ros_x: Stopped implementing pong.cpp because I learned enough about InteractiveMarker.

--- ros_x/catkin_ws/src/using_markers/src/pong.cpp
+++ ros_x/catkin_ws/src/using_markers/src/pong.cpp
@@ -26,9 +26,127 @@ public:
         makeFieldMarker();
         makePaddleMarkers();
         makeBallMarker();
+
+        reset();
+        updateScore();
+
+        ros::NodeHandle nh;
+        game_loop_timer_ = nh.createTimer( ros::Duration(UPDATE_RATE),
+                                           boost::bind(&PongGame::spinOnce, 
+                                                       this) );
     }
 
 private:
+    void reflect( float &pos,
+                  float last_pos,
+                  float limit,
+                  float &t )
+    {
+        if( pos > limit )
+        {
+            t = (pos - limit) /
+                (pos - last_pos);
+            return true;
+        }
+
+        if( -pos > limit )
+        {
+            t = (-pos - limit) /
+                (last_pos - pos);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    void spinOnce()
+    {
+        if( player_contexts_[0].active ||
+            player_contexts_[1].active )
+        {
+            float ball_dx = speed * ball_dir_x_;
+            float ball_dy = speed * ball_dir_y_;
+
+            ball_pos_x_ += ball_dx;
+            ball_pos_y_ += ball_dy;
+
+            float t = 0;
+            if( reflect(ball_pos_y_,
+                        last_ball_pos_y_,
+                        FIELD_HEIGHT * 0.5,
+                        t) )
+            {
+                ball_pos_x -= t * ball_dx;
+                ball_pos_y -= t * ball_dy;
+
+                ball_dir_y_ *= -1.0;
+
+                ball_dx = speed * ball_dir_x_;
+                ball_dy = speed * ball_dir_y_;
+
+                ball_pos_x_ += t * ball_dx;
+                ball_pos_y_ += t * ball_dy;
+            }
+        }
+    }
+
+    void updateScore()
+    {
+        InteractiveMarker int_marker;
+        int_marker.header.frame_id = "base_link";
+        int_marker.name = "score";
+
+        InteractiveMarkerControl control;
+        control.always_visible = true;
+
+        Marker marker;
+        marker.type = Marker:TEXT_VIEW_FACING;
+        marker.color.r = 1.0;
+        marker.color.g = 1.0;
+        marker.color.b = 1.0;
+        marker.color.a = 1.0;
+        marker.scale.x = 1.5;
+        marker.scale.y = 1.5;
+        marker.scale.z = 1.5;
+
+        std::ostringstream s;
+        s << player_contexts_[0].score;
+        marker.text = s.str();
+        marker.pose.position.x = -1.0 * ( FIELD_WIDTH * 0.5 + BORDER_SIZE );
+        marker.pose.position.y = FIELD_HEIGHT * 0.5 + 4.0 * BORDER_SIZE;
+        control.markers.push_back( marker );
+
+        s.str("");
+        s << player_contexts_[1].score;
+        marker.text = s.str();
+        marker.pos.position.x *= -1;
+        control.markers.push_back( marker );
+
+        int_marker.controls.push_back( control );
+
+        server_.insert( int_marker );
+    }
+
+    void reset()
+    {
+        speed_ = 6.0 * UPDATE_RATE;
+        ball_pos_x_ = 0.0;
+        ball_pos_y_ = 0.0;
+        ball_dir_x_ = ball_dir_x_ > 0.0 ? 1.0 : -1.0;
+        ball_dir_y_ = rand() % 2 ? 1.0 : -1.0;
+        normalizeVel();
+    }
+
+    void normalizeVel()
+    {
+        float l = sqrt( ball_dir_x_ * ball_dir_x_ +
+                        ball_dir_y_ * ball_dir_y_ );
+
+        ball_dir_x_ /= 1;
+        ball_dir_y_ /= 1;
+    }
+
     void makeFieldMarker()
     {
         InteractiveMarker int_marker;
@@ -178,4 +296,34 @@ private:
         server_.insert( int_marker );
     }
 
+    interactive_markers::InteractiveMarkerServer server_;
+    ros::Timer game_loop_timer_;
+
+    InteractiveMarker field_marker_;
+
+    struct PlayerContext
+    {
+        PlayerContext():
+            pos(0),
+            active(false),
+            score(0)
+        {
+        }
+
+        float pos;
+        bool active;
+        int score;
+    };
+
+    std::vector<PlayerContext> player_contexts_;
+
+    float last_ball_pos_x_;
+    float last_ball_pos_y_;
+
+    float ball_pos_x_;
+    float ball_pos_y_;
+
+    float ball_dir_x_;
+    float ball_dir_y_;
+    float speed_;
 };
