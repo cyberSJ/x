ddb1f4b00ef68be8d49c744f6e0a9d572a7aff33
sung <sungjoonkim4.0@gmail.com>
2017-12-14 22:47:39 -0500

    log4j_x: Added procedures on how to run the example code.

--- log4jExample.java
+++ log4jExample.java
@@ -1,4 +1,10 @@
+// Code obtained from
+// https://www.tutorialspoint.com/log4j/log4j_logging_files.htm
+// Make sure the log4j.properties path is in the PATH environment variable.
 // Assumes the log4j jar is in the CLASSPATH.
+// Then to run this:
+// 1. javac log4jExample.java
+// 2. java log4jExample
 import org.apache.log4j.Logger;
 
 import java.io.*;
