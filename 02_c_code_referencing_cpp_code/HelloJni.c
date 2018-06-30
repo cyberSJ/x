fe4be404c9b9d942f98a046cbb56400f7cae5ee7
cyberSJ <sungjoonkim4.0@gmail.com>
2018-06-30 19:23:52 -0400

    jni_x: Refactored the way .cpp file is compiled.

--- 02_c_code_referencing_cpp_code/HelloJni.c
+++ 02_c_code_referencing_cpp_code/HelloJni.c
@@ -1,9 +1,5 @@
 #include "HelloJniImpl.h"
 
-// Include the implementation. Otherwise, we would have to create a C++ library
-// for the sayHello function and link it when we compile this C code.
-#include "HelloJniImpl.cpp"
-
 /**
  * This c program will reference a header file that contains yet another
  * implementation of saying hello. That implementation will be defined in cpp
--- 02_c_code_referencing_cpp_code/sung_run.sh
+++ 02_c_code_referencing_cpp_code/sung_run.sh
@@ -9,12 +9,10 @@ javac -h . HelloJni.java
 # Create a shared library. The JNI implementation is in HelloJni.c, but that
 # code uses another custom function declared in a C++ code. That custom function
 # is declared in HelloJniImpl.h, and the implementation of it is in 
-# HelloJniImpl.cpp file. The C code references both the custom .h and .cpp files
-# so we do not have to link a library when compiling the C code.
-# Since the C code references a C++ code, we need to use g++ instead of gcc.
+# HelloJniImpl.cpp file. So enter the .c and .cpp files in the argument.
 # -fPIC: The generated shared library is independent of which address it is
 #    loaded to.
-g++ -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" --shared -o libhello.so HelloJni.c
+g++ -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" --shared -o libhello.so HelloJni.c HelloJniImpl.cpp
 
 # Run the java program that uses JNI to call C function
 # -Djava.library.path=.: The shared library that JNI needs is in current
